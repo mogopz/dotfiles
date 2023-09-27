efeee63e2e99d26ee77ed1934dbacc5a02ebeb19
mog <github@moop.dev>
2023-09-27 11:37:29 +1000

    update brew config

--- setup.sh
+++ setup.sh
@@ -20,9 +20,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Setup symlinks
 ln -sf "$(pwd)/dotfiles/git/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/dotfiles/git/.gitignore" ~/.gitignore
-
 ln -sf "$(pwd)/dotfiles/spacevim/init.toml" ~/.SpaceVim.d/init.toml
 ln -sf "$(pwd)/dotfiles/starship/starship.toml" ~/.config/starship.toml
-
 ln -sf "$(pwd)/dotfiles/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/dotfiles/.zshrc" ~/.zshrc
+mkdir -p ~/.config/lsd
+ln -sf "$(pwd)/dotfiles/lsd/config.yaml" ~/.config/lsd/config.yaml
