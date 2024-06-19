b3972298df455d4a64e08bf09bef519c5a895864
mog <github@moop.dev>
2024-06-19 16:30:14 +1000

    fix: update vscode config

--- .zshrc
+++ .zshrc
@@ -1,11 +1,15 @@
-export ZSH="/Users/mog/.oh-my-zsh"
-export PATH="${PATH}:$(go env GOPATH)/bin"
+autoload -Uz compinit
+compinit
 
 export AWS_PAGER=""
 export ARGOCD_OPTS="--grpc-web"
 export HISTCONTROL="ignoreboth"
-export KUBECTL_EXTERNAL_DIFF="delta --paging never"
+export BAT_THEME="Catppuccin Mocha"
+export EDITOR="nvim"
 export KUBE_EDITOR="nvim"
+export KUBECTL_EXTERNAL_DIFF="delta --paging never"
+export SUDO_EDITOR="${EDITOR}"
+export WORDCHARS=${WORDCHARS//-/}
 
 # Coloured man pages
 export LESS_TERMCAP_mb=$'\E[01;31m'
@@ -16,34 +20,26 @@ export LESS_TERMCAP_so=$'\E[38;33;246m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[04;38;5;146m'
 
-plugins=(
-  aws
-  git
-  kubectl
-  macos
-  httpie
-  z
-)
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh
+source <(fzf --zsh)
 
-source "${ZSH}/oh-my-zsh.sh"
-source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-source <(switcher init zsh)
-
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
 alias k="kubecolor"
-alias kctx="switch"
-alias kns="switch ns"
+alias kns="kubens"
+alias kctx="kubectx"
 alias ll="ls -lah"
 alias ls="lsd"
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias vim="nvim"
-alias wget="wget --no-hsts"
+
+alias gaa="git add --all"
+alias gcmsg="git commit --message"
+alias ggp="git push"
+
+alias glods='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset" --date=short'
+
 
 compdef kubecolor=kubectl
 
 eval "$(fnm env --use-on-cd)"
 eval "$(starship init zsh)"
-eval "$(atuin init zsh --disable-up-arrow)"
--- setup.sh
+++ setup.sh
@@ -5,10 +5,7 @@ set -eo pipefail
 # Setup Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor
-brew bundle
-
-# Setup Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew bundle --file "${HOME}/dotfiles/Brewfile"
 
 # Setup TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -17,25 +14,27 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 cat <<EOF > ~/.config/nvim/lua/plugins/theme.lua
 return {
-	{ "Mofiqul/dracula.nvim" },
-
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "dracula",
-		},
-	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    }
+  }
 }
 EOF
 
+# Setup Zsh highlighting
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git catppuccin-zsh-syntax-highlighting
+cp -v catppuccin-zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/catppuccin-mocha-zsh-syntax-highlighting.zsh
+rm -rf catppuccin-zsh-syntax-highlighting
+
 # Setup symlinks
 mkdir -p ~/.config/lsd
-mkdir -p ~/.config/alacritty
+ln -sf "$(pwd)/dotfiles/ghostty/config.toml" "${HOME}/Library/Application Support/com.mitchellh.ghostty/config"
 ln -sf "$(pwd)/dotfiles/git/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/dotfiles/git/.gitignore" ~/.gitignore
-ln -sf "$(pwd)/dotfiles/alacritty/dracula.toml" ~/.config/alacritty/dracula.toml
-ln -sf "$(pwd)/dotfiles/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
 ln -sf "$(pwd)/dotfiles/lsd/config.yaml" ~/.config/lsd/config.yaml
+ln -sf "$(pwd)/dotfiles/lsd/colors.yaml" ~/.config/lsd/colors.yaml
 ln -sf "$(pwd)/dotfiles/starship/starship.toml" ~/.config/starship.toml
 ln -sf "$(pwd)/dotfiles/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/dotfiles/.zshrc" ~/.zshrc
